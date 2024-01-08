f835277ab5cf676856fa31e64b2be6c4a9a2b4ff
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-09 02:39:00 +0400

    Initial Commit

---
+++ Spotify Player/Spotify_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_PlayerApp.swift
+//  Spotify Player
+//
+//  Created by Jaiveer Chadda on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
